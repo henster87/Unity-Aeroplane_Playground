c0ac28977e20707da439d599e3facc4d0031a6df
henster87 <henry-may@hotmail.fr>
2023-05-14 23:52:05 +0200

    Challenge 1 - Aeroplane Playground

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    static private float propellerSpeed = 80.0f;
+    private Vector3 speed = new Vector3(0.0f, 0.0f, propellerSpeed);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed);
+    }
+}
